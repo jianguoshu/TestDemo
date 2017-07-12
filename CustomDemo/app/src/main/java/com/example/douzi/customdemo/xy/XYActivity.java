ec77a7ea9c083a3f0311f5be572781ca5d94114d
zhuxiaohui <douzi@zhuxiaohuideMacBook-Pro.local>
2017-07-12 16:36:45 +0800

    1. x，y坐标

---
+++ CustomDemo/app/src/main/java/com/example/douzi/customdemo/xy/XYActivity.java
@@ -0,0 +1,98 @@
+package com.example.douzi.customdemo.xy;
+
+import android.app.Activity;
+import android.graphics.Rect;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.douzi.customdemo.R;
+
+public class XYActivity extends Activity {
+    private View one;
+    private View two;
+    private View three;
+    private View threeChild;
+    private boolean hasChanged = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xy);
+
+
+        one = this.findViewById(R.id.tv_one);
+        two = this.findViewById(R.id.tv_two);
+        three = this.findViewById(R.id.tv_three);
+        threeChild = this.findViewById(R.id.tv_three_child);
+
+        this.findViewById(R.id.tv_reset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                XYActivity.this.one.requestLayout();
+                XYActivity.this.two.requestLayout();
+                XYActivity.this.three.requestLayout();
+                XYActivity.this.threeChild.requestLayout();
+                threeChild.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        printXYInfo("reset");
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        changeXY();
+        super.onWindowFocusChanged(hasFocus);
+    }
+
+    private void changeXY() {
+        if (!hasChanged) {
+            printXYInfo("default");
+            one.offsetLeftAndRight(50); // 修改mLeft、mRight（重新布局后恢复）
+            one.offsetTopAndBottom(50); // 修改mTop、mBottom的值（重新布局后恢复）
+            two.setTranslationX(50); // 修改TranslationX属性
+            two.setTranslationY(50); // 修改TranslationY属性
+            three.scrollBy(-50, -50);// 修改mScrollX、mScrollY属性
+            printXYInfo("change");
+            hasChanged = true;
+        }
+    }
+
+    private void printXYInfo(String divider) {
+        Rect oneLocalVisibleRect = new Rect();
+        one.getLocalVisibleRect(oneLocalVisibleRect);
+        Rect oneGlobalVisibleRect = new Rect();
+        one.getGlobalVisibleRect(oneGlobalVisibleRect);
+        int[] oneLocationInWindow = new int[2];
+        one.getLocationInWindow(oneLocationInWindow);
+        int[] oneLocationOnScreen = new int[2];
+        one.getLocationOnScreen(oneLocationOnScreen);
+        Rect twoLocalVisibleRect = new Rect();
+        two.getLocalVisibleRect(twoLocalVisibleRect);
+        Rect twoGlobalVisibleRect = new Rect();
+        two.getGlobalVisibleRect(twoGlobalVisibleRect);
+        int[] twoLocationInWindow = new int[2];
+        two.getLocationInWindow(twoLocationInWindow);
+        int[] twoLocationOnScreen = new int[2];
+        two.getLocationOnScreen(twoLocationOnScreen);
+        Rect threeLocalVisibleRect = new Rect();
+        three.getLocalVisibleRect(threeLocalVisibleRect);
+        Rect threeGlobalVisibleRect = new Rect();
+        three.getGlobalVisibleRect(threeGlobalVisibleRect);
+        int[] threeLocationInWindow = new int[2];
+        three.getLocationInWindow(threeLocationInWindow);
+        int[] threeLocationOnScreen = new int[2];
+        three.getLocationOnScreen(threeLocationOnScreen);
+        Rect threeChildLocalVisibleRect = new Rect();
+        threeChild.getLocalVisibleRect(threeChildLocalVisibleRect);
+        Rect threeChildGlobalVisibleRect = new Rect();
+        threeChild.getGlobalVisibleRect(threeChildGlobalVisibleRect);
+        int[] threeChildLocationInWindow = new int[2];
+        threeChild.getLocationInWindow(threeChildLocationInWindow);
+        int[] threeChildLocationOnScreen = new int[2];
+        threeChild.getLocationOnScreen(threeChildLocationOnScreen);
+    }
+}
